b5dd20d7411b1be9dd254d0111d3d7423f53eb9e
Su <sandar@solidplm.com>
2020-02-21 09:44:50 +0630

    Add Constructor and Parse Parameter

---
+++ GradeBook/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    class Book
+    {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        private List<double> grades;
+        private string name;
+    }
+}
